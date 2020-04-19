09cc896846b9f20d8426742cf4b05c373b720215
ZePotente <ezeposada@gmail.com>
2020-04-19 17:18:42 -0300

    Alguna cosa.

--- Receptor/Client/src/modelo/InternetManager.java
+++ Receptor/Client/src/modelo/InternetManager.java
@@ -25,7 +25,6 @@ public class InternetManager {
                     conexionExitosa(); // metodo de IMR
                     while (true) { // una vez que escucha ese puerto se queda escuchandolo aunque ingresen otro puerto
                         Socket soc = s.accept();
-                        PrintWriter out = new PrintWriter(soc.getOutputStream(), true); // hace falta?
                         BufferedReader in = new BufferedReader(new InputStreamReader(soc.getInputStream()));
                         
                         // se podran enviar en XML o alguna otra forma?
@@ -33,7 +32,7 @@ public class InternetManager {
                         msg = "";
                         aux = in.readLine(); //Lee todo el principio y el primer parrafo del mensaje
                         // System.out.println("Primera linea: \n" + aux);
-                        while (aux != "" && aux != null) {
+                        while (aux != null) {
                             msg += aux + "\n"; // cada "\n" separa parrafos del mensaje
                             // cada readLine() lee un renglon del mensaje,
                             // y el ultimo lee el ultimo parrafo y todo el resto
@@ -41,7 +40,7 @@ public class InternetManager {
                             // System.out.println(msg);
                         }
                         mensajeRecibido(msg); // metodo de IMR
-                        // in. close(); out.close(); soc.close(); // deberian?
+                        soc.close(); // deberian?
                     }
                 } catch (Exception e) {
                     errorConexion(e.getMessage()); // metodo de IMR
